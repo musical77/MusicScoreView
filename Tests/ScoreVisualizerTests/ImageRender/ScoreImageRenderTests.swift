9befdbacc2cfa8ab3c83d9c3b8adcf52ad3cd7c1
live77 <live77>
2022-02-04 13:00:28 +0800

    add test for render images

--- Tests/ScoreVisualizerTests/ImageRender/ScoreImageRenderTests.swift
+++ Tests/ScoreVisualizerTests/ImageRender/ScoreImageRenderTests.swift
@@ -6,11 +6,21 @@ import MusicScore
 
 class ScoreImageRenderTests: XCTestCase {
 
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
     func testScoreRender1() {
         let testScore = ScoreSamples.chopin92
         let render = ScoreImageRender(score: testScore)
         let image = render.render()
         
+        if let data = image.pngData() {
+            let fileURL = getDocumentsDirectory().appendingPathComponent("render.png")
+            try? data.write(to: fileURL)
+            print(fileURL)
+        }
     }
 
 }
