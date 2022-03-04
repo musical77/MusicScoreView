9d098ef5436065429e44e70fb50dabf0d0279bdd
live77 <live77>
2022-03-04 20:09:12 +0800

    refine example view

--- Example/ScoreViewExample/ScoreViewExample/ContentView.swift
+++ Example/ScoreViewExample/ScoreViewExample/ContentView.swift
@@ -12,26 +12,26 @@ import MusicScoreView
 
 struct ExampleView: View {
     
-    /// use midi file url as you wish
+    /// replace it with a midi file url as you wish
     static let midiURL = ScoreSamples.url_spring1st
     
     /// load midi to `MusicScore` then loaded into a `ScoreViewModel`
     @ObservedObject var scoreVM = ScoreViewModel(score: MusicScore(url: midiURL)!)
-    @State var beatAt: MusicTimeStamp = 0.0
 
     var body: some View {
         VStack {
             ScoreView(viewModel: scoreVM)
         }.onReceive(timer) { t in
-            beatAt = beatAt + 0.015
-            scoreVM.seek(beginBeat: beatAt, endBeat: beatAt + 16.0)
+            beginBeat = beginBeat + 0.015
+            scoreVM.seek(beginBeat: beginBeat, endBeat: beginBeat + 16.0)
         }
     }
     
     /// change begin beat periodically
     let timer = Timer.publish(every: 0.015, on: .main, in: .common).autoconnect()
+    
+    @State var beginBeat: MusicTimeStamp = 0.0
 }
-
 
 struct Example_Previews: PreviewProvider {
     static var previews: some View {
